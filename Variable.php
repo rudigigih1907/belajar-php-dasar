23792424c3b32375bce5048f10e56c2323c48355
rudigigih1907 <rudigigih0719@gmail.com>
2023-01-18 14:58:24 +0700

    Variable

---
+++ Variable.php
@@ -0,0 +1,24 @@
+<?php
+$name = "Rudi Gigih Prabowo";
+$$name = "Keren";
+$age = 30;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Umur : ";
+echo $age;
+echo "\n";
+
+// Variable Variables
+$contoh = "rudi";
+$$contoh = "prabowo";
+
+echo "Contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "rudi : ";
+echo $rudi;
+echo "\n";
