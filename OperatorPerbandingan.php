ebd48894232e2655ddb60e004ceda4dbbaf7a477
rudigigih1907 <rudigigih0719@gmail.com>
2023-03-01 10:47:07 +0700

    Operator Perbandingan

---
+++ OperatorPerbandingan.php
@@ -0,0 +1,7 @@
+<?php
+
+var_dump("10" == 10); // cek apakah nilainya sama, bernilai true jika nilainya sama walaupun type datanya berbeda.
+var_dump("10" === 10); // cek apakah nilai dan type datanya sama, bernilai true jika nilai dan type data sama.
+
+var_dump(10 < 9);
+var_dump(20 > 10);
