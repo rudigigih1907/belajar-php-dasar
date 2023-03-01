4c6122baca4e551ac1262d16d5bc38167ae8aeb6
rudigigih1907 <rudigigih0719@gmail.com>
2023-03-01 10:36:09 +0700

    Operator Penugasan Aritmatika

---
+++ OperatorPenugasan.php
@@ -0,0 +1,13 @@
+<?php
+
+$total = 0;
+
+$fruit = 5000;
+$chicken = 10000;
+$orangeJuice = 5000;
+
+$total = $total + $fruit;
+$total = $total + $chicken;
+$total = $total + $orangeJuice;
+
+var_dump($total);
