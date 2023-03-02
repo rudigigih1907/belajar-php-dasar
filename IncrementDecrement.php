93bc794af405a91380530dff4f13703465763757
rudigigih1907 <rudigigih0719@gmail.com>
2023-03-02 11:35:52 +0700

    IncrementDecrement

---
+++ IncrementDecrement.php
@@ -0,0 +1,11 @@
+<?php
+// $a++; Post Increment Kembalikan $a lalu naikan 1 angka 
+// ++$a; Pre Increment Naikan $a lalu kembalikan $a
+// $a--; Post Decrement Kembalikan $a lalu turunkan 1 angka
+// --$a; Pre Decrement Turunkan $a satu angka, lalu kembalikan $a
+$a = 10;
+
+$b = ++$a;
+
+var_dump($a);
+var_dump($b);
